06e0431bce7a2cd82f4d6f9de554d8ff958b3872
windard <windard@qq.com>
2019-03-05 10:17:04 +0800

    polish

--- 231.power-of-two.py
+++ 231.power-of-two.py
@@ -41,7 +41,7 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        if n == 0:
+        if n <= 0:
             return False
         return (n & n - 1) == 0
 
@@ -66,4 +66,3 @@ class Solution(object):
                 return False
             n = n / 2
         return True
-
