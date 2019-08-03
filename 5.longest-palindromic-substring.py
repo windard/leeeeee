d0d26ba41a9adb2aabaf5bb77f083c0446f8b132
windard <windard@qq.com>
2019-08-03 21:32:38 +0800

    MLA 马拉车算法

--- 5.longest-palindromic-substring.py
+++ 5.longest-palindromic-substring.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 #
 # @lc app=leetcode id=5 lang=python
 #
@@ -36,17 +37,85 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # 马拉车算法
-        # 将奇偶两种情况转换为一种情况来处理
-        # Manacher's Algorithm
-        # T(n) = O(n)
-        # 最长子串的长度是半径减1，起始位置是中间位置减去半径再除以2。
+        if not s:
+            return s
+
         ls = "$#"
         for i in s:
             ls += i
             ls += "#"
         ls += "&"
+        data = {}
 
+        max_index = 0
+        right_max_index = 0
+        data[right_max_index] = 0
+        for index in range(len(ls)):
+            init = 0
+            if index <= right_max_index + data[right_max_index]:
+                symmetry_idnex = 2*right_max_index - index
+                # 中线往后的点，当前位置加对称点的半径仍然小于右边界可直接设值
+                if index - right_max_index + data[symmetry_idnex] < data[right_max_index]:
+                    data[index] = data[symmetry_idnex]
+                    continue
+                # 中线往后的点，当前位置加对称点的半径仍然等于右边界可以从对称点的半径开始计算
+                elif index - right_max_index + data[symmetry_idnex] == data[right_max_index]:
+                    init = data[symmetry_idnex]
+                # 中线往后的点，当前位置加对称点的半径大于右边界，可以从右边界开始减去当前位置当作半径开始计算
+                else:
+                    init = right_max_index + data[right_max_index] - index
+            while index - index >= 0 and init + index < len(ls):
+                if ls[index-init] == ls[index+init]:
+                    init += 1
+                else:
+                    break
+            data[index] = init - 1
+            if data[index] + index > data[right_max_index] + right_max_index:
+                right_max_index = index
+            if data[index] > data[max_index]:
+                max_index = index
+        
+        return ls[max_index-data[max_index]:max_index+data[max_index]+1].replace("#", "")
+
+    def __longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # 马拉车算法
+        # 将奇偶两种情况转换为一种情况来处理
+        # Manacher's Algorithm
+        # T(n) = O(n)
+        # 最长子串的长度是半径减1，起始位置是中间位置减去半径再除以2。
+
+        # 走一半力，借一半力，不能完全依赖之前的结果
+        # 比如 ababababa
+        # Wrong Answer
+        if not s:
+            return s
+
+        ls = "$#"
+        for i in s:
+            ls += i
+            ls += "#"
+        ls += "&"
+        data = {}
+
+        max_index = 0
+        right_max_index = 0
+        data[right_max_index] = 0
+        for index, _ in enumerate(ls):
+            if index < right_max_index + data[right_max_index] / 2:
+                    data[index] = data[right_max_index - (index - right_max_index)]
+                    continue
+
+            ts = self.checkSinglePalindromic(ls, index)
+            data[index] = len(ts) / 2
+            if index + data[index] > right_max_index + data[right_max_index]:
+                right_max_index = index
+                if data[right_max_index] > data[max_index]:
+                    max_index = right_max_index
+        return ls[max_index-data[max_index]:max_index+data[max_index]+1].replace("#","")
 
     def _longestPalindrome(self, s):
         """
@@ -90,3 +159,12 @@ class Solution(object):
                 break
         return group
 
+# if __name__ == "__main__":
+#     s = Solution()
+#     print s.longestPalindrome("babadada")
+#     print s.longestPalindrome("ababababa")
+#     test_case = ["", "abcdcef", "adaelele", "cabadabae", "aaaabcdefgfedcbaa", "aaba", "aaaaaaaaa"]
+#     test_case = ["aaaaaaaaa"]
+#     for t in test_case:
+#         print s.longestPalindrome(t)
+#         print 
